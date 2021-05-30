27dd7bfd946658cc1252a509fbda065d35c911ac
EdSabino <eduardoaikin@gmail.com>
2021-05-30 20:26:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import img from './asd.png';
+import React, { useState } from 'react';
 
 function App() {
+  const [image, setImage] = useState();
+  const [open, setOpen] = useState(false);
+
+  const openImage = (item) => {
+    setImage(item);
+    setOpen(true);
+  }
+
+  const squares = (line) => {
+    const vec = [];
+    for (let i =0; i < 5; i++) {
+      const item = 75 - ((line * 5) + (5 - i));
+      vec.push(<div className="Col" onClick={() => openImage(item)} ></div>)
+    }
+    return vec
+  }
+
+  const rows = () => {
+    const vec = [];
+    for (let i = 0; i < 15; i++) {
+      vec.push(<div className="Row">{ squares(i) }</div>)
+    }
+    return vec
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Board">
+        { rows() }
+      </div>
+      <div className={open ? "modal open" : "modal"} onClick={() => setOpen(false)}>
+        <img src={img} alt="test" className="modal-content" id="img01" />
+      </div>
     </div>
   );
 }
